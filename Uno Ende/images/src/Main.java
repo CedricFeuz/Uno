49feb9411773f944122f4c958be78e18ed73de63
cedri <cedri@DESKTOP-R5ADSC9>
2019-10-13 21:36:36 +0200

    Ein Uno-Projekt

---
+++ Uno Ende/images/src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.ImageIcon;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+/**
+ * Uno Gameboard
+ *
+ * @author Cédric Feuz
+ * @since 2019-06-15
+ * @version 1.0
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Startmenu start = new Startmenu();
+		//PlayerNameChoose playername = new PlayerNameChoose();
+		//Gameboard board = new Gameboard();
+	}
+}
---
+++ Uno Ende/src/model/NumCard.java
@@ -0,0 +1,17 @@
+package model;
+import javax.swing.ImageIcon;
+
+public class NumCard extends CardModel {
+
+	public NumCard(int num, int color, ImageIcon img) {
+		super(num, color, img);
+	}
+
+	@Override
+	public void effect() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
